368d5b461e089faa2b0e53a462a783c612553ea9
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2025-06-29 13:02:47 +0530

    changes

--- dist/app/modules/offer-letter/offer-letter.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.offerLetterStatus = void 0;
-var offerLetterStatus;
-(function (offerLetterStatus) {
-    offerLetterStatus["DRAFT"] = "draft";
-    offerLetterStatus["SENT"] = "send";
-    offerLetterStatus["FAILED"] = "failed";
-})(offerLetterStatus || (exports.offerLetterStatus = offerLetterStatus = {}));
